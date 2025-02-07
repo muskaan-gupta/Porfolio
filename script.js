784cd8abeb2535dbb13a02fd45cce37dcefc6fba
muskaan-gupta <muskangupta7414@gmail.com>
2025-02-07 20:00:17 +0530

    edited responsiveness and skill section

--- script.js
+++ script.js
@@ -78,6 +78,9 @@ const form = document.forms["submit-to-google-sheet"];
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {
+      console.log(form);
+      console.log("Success!", response);
+    })
     .catch((error) => console.error("Error!", error.message));
 });
